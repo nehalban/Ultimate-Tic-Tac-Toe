e9ad09d7c5a51753535b5fd47befb6976c4670e8
Nehal <nehalbansal@outlook.com>
2026-03-07 00:03:02 +0530

    ult_ttt rules

--- ultimate_ttt.cpp
+++ ultimate_ttt.cpp
@@ -19,29 +19,55 @@ struct ult_ttt{
             }
             else{
                 cout<<"You can play in any big block."<<endl;
-                cout<<"Enter row and column of big block: "<<endl;
-                cin>>big_row>>big_col;
+                
+                bool valid = 0;
+                while(!valid){
+                    cout<<"Enter row and column of big block: "<<endl;
+                    cin>>big_row>>big_col;
+                    if(big_row<0 || big_row>2 || big_col<0 || big_col>2){
+                        cout<<"Invalid big block. Row and column must be between 0 and 2."<<endl;
+                    }
+                    else if(big_board.board_filled[big_row][big_col]){
+                        cout<<"Invalid big block. This block is already won by "<<(big_board.board[big_row][big_col]?'X':'O')<<"."<<endl;
+                    }
+                    else if(!big_board.board[big_row][big_col] && !big_board.board_filled[big_row][big_col]){
+                        cout<<"Invalid big block. This block is already drawn."<<endl;
+                    }
+                    else valid = 1;
+                }
                 cout<<"You chose to play in the big block ("<<big_row<<","<<big_col<<")"<<endl;
             }
             cout<<"Enter row and column of small block: "<<endl;
             cin>>row>>col;
             cout<<endl;
             if(small_boards[big_row][big_col].move(row, col, curr_player)){
+                bool full_check [3][3] = {{1,1,1},{1,1,1},{1,1,1}};
                 if(small_boards[big_row][big_col].game_over(row, col)){
+                    big_board.board_filled[big_row][big_col] = 1;
                     big_board.move(big_row, big_col, curr_player);
                     if(big_board.game_over(big_row, big_col)){
-                        draw=0;
-                        break;
+                        cout<<(curr_player?'X':'O')<<" wins the game!"<<endl;
+                        return;
                     }
                 }
+                else if(small_boards[big_row][big_col].board_filled ==  full_check){
+                    //big_board.board_filled[big_row][big_col] = 0;
+                    big_board.board[big_row][big_col] = 1;
+                }
                 curr_player = !curr_player;
+                if(big_board.board_filled[row][col]== 0 && big_board.board[row][col]== 0 ){
+                    big_block_fixed = 0;
+                }
+                else{
                 big_row = row;
                 big_col = col;
                 big_block_fixed = 1;
+                }
                 total_moves++;
             }
         }
     }
+    
     void print_board(){
         for (int br = 0; br < 3; br++) {
             for (int r = 0; r < 3; r++) {
